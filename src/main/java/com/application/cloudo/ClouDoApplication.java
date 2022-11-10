9297699ce2efc2ad5ae930188ac831cd814659ea
KangSungBum <peter9706@naver.com>
2022-11-10 14:30:34 +0900

    Project Initial Commit

---
+++ src/main/java/com/application/cloudo/ClouDoApplication.java
@@ -0,0 +1,13 @@
+package com.application.cloudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClouDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/application/cloudo/ClouDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.cloudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClouDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
