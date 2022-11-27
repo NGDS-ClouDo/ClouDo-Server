e9ebdc16fb1548b2ccbc0741b393171839c10cd5
KangSungBum <peter9706@naver.com>
2022-11-27 16:36:20 +0900

    feat(RecordRepository.java): 완료 기록 조회 기능 추가

--- src/main/java/com/application/cloudo/repository/RecordRepository.java
+++ src/main/java/com/application/cloudo/repository/RecordRepository.java
@@ -2,10 +2,44 @@ package com.application.cloudo.repository;
 
 import com.application.cloudo.domain.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
-    List<Record> findRecordByUserOrderByCreatedDateAsc();
+    @Query("select r " +
+            "from Record r " +
+            "where r.user.name = :name " +
+            "order by r.createdDate asc")
+    List<Record> findRecordByUserNameOrderByCreatedDateAsc(
+            @Param("name") String name
+    );
+
+    @Query("select r " +
+            "from Record r " +
+            "where r.user.name = :name " +
+            "order by r.createdDate desc")
+    List<Record> findRecordByUserNameOrderByCreatedDateDesc(
+            @Param("name") String name
+    );
+
+    @Query("select r " +
+            "from Record r " +
+            "where r.recordDone = 1 " +
+            "and r.user.name = :name " +
+            "order by r.createdDate asc")
+    List<Record> findFinishedRecordByUserNameOrderByCreatedDateAsc(
+            @Param("name") String name
+    );
+
+    @Query("select r " +
+            "from Record r " +
+            "where r.recordDone = 1 " +
+            "and r.user.name = :name " +
+            "order by r.createdDate asc")
+    List<Record> findFinishedRecordByUserNameOrderByCreatedDateDesc(
+            @Param("name") String name
+    );
 }
